a1de900ac00ba5798643d9f3610582c895f87a7f
wondersmasher <wonderfulakinniyi@gmai.com>
2023-02-04 19:31:36 +0100

    Implemented useFetch and fetched data for weather and forecast.

---
+++ src/Components/useFetch.js
@@ -0,0 +1,27 @@
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+const useFetch = (url) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const getWeatherForecast = useCallback(async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+
+      setLoading(false);
+      setError(false);
+    } catch (error) {
+      console.error(error.message);
+      setLoading(false);
+      setError(true);
+    }
+  }, [url]);
+  useEffect(() => {
+    getWeatherForecast();
+  }, [getWeatherForecast, url]);
+  return { data, loading, error };
+};
+
+export default useFetch;
